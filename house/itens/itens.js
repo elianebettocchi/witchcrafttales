702b1bf7d9c34295f4f441fd6d3d3ee2f8080b44
elianebettocchi <116610216+elianebettocchi@users.noreply.github.com>
2023-03-16 17:20:48 -0400

    Add files via upload

---
+++ house/itens/itens.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('itens').addEventListener('click', function () {
+        let drawValue = Math.floor(Math.random() * 25)
+        document.getElementById('itens').src = `./cards/${drawValue}.png`
+
+    })
+
+    addEventListener('click', function () {
+        allDraws = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    })
+})

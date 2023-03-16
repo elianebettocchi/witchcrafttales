b3ca2af761f74f5414f4faf25b50903d41c144c1
elianebettocchi <116610216+elianebettocchi@users.noreply.github.com>
2023-03-16 15:37:52 -0400

    Add files via upload

---
+++ bonds/bonds.js
@@ -0,0 +1,24 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('events').addEventListener('click', function () {
+        let rolledValue = Math.floor(Math.random() * 43)
+        document.getElementById('events').src = `./events/${rolledValue}.png`
+
+    })
+
+    addEventListener('click', function () {
+        allRolls = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+    })
+})
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('roles').addEventListener('click', function () {
+        let drawValue = Math.floor(Math.random() * 6)
+        document.getElementById('roles').src = `./roles/${drawValue}.png`
+
+    })
+
+    addEventListener('click', function () {
+        allDraws = [0, 0, 0, 0, 0, 0]
+    })
+})

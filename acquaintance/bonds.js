da7e479c8a49989350a1dd5bdca3479bf2bc1aa9
Eliane Bettocchi <elianebettocchi@gmail.com>
2023-06-16 15:22:26 -0400

    Add files via upload

--- acquaintance/bonds.js
+++ acquaintance/bonds.js
@@ -1,17 +1,5 @@
 
 document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('events').addEventListener('click', function () {
-        let rolledValue = Math.floor(Math.random() * 43)
-        document.getElementById('events').src = `./events/${rolledValue}.png`
-
-    })
-
-    addEventListener('click', function () {
-        allRolls = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-
-    })
-})
-document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('roles').addEventListener('click', function () {
         let drawValue = Math.floor(Math.random() * 16)
         document.getElementById('roles').src = `./roles/${drawValue}.png`

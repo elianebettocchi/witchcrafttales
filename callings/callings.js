2eea0ab0e264f06002fc6d41d43465151cc59b29
Eliane Bettocchi <elianebettocchi@gmail.com>
2023-07-17 12:37:12 -0400

    Add files via upload

---
+++ callings/callings.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('callings').addEventListener('click', function () {
+        let drawValue = Math.floor(Math.random() * 24)
+        document.getElementById('callings').src = `./cards/${drawValue}.png`
+
+    })
+
+    addEventListener('click', function () {
+        allDraws = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    })
+})
+var x = document.getElementById("myAudio"); 
+
+function playAudio() { 
+  x.play(); 
+} 
